36beb78cf9c1e9656cfbed94436060b4c10e1199
Evan Ogas <evanogas@gmail.com>
2015-07-11 10:41:20 -0500

    basic js injection into active tab

--- refind.js
+++ refind.js
@@ -2,24 +2,25 @@
 /* global chrome */
 
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("popup onload");
-    
     var searchBox = document.getElementById("refind-input");
     var searchButton = document.getElementById("refind-submit");
     
     searchButton.onclick = function(ev) {
-        console.log("searchButton.onclick");
-        
         chrome.tabs.query({
             active: true,
             currentWindow: true
         }, function(tabs) {
             if (tabs.length < 1) {
+                // No active tab, fail!
                 return;
             }
             
+            // We should only ever get one tab back
             var currentTab = tabs[0];
-            console.log(currentTab);
+            
+            chrome.tabs.executeScript(currentTab.id, {
+                file: "inject.js"
+            });
         });
     };
 });
